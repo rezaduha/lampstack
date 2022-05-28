86a0c74f5628dbb779a70c3aa928c6d1ee8eb42c
rezaduha <rezafauzizisiwaduha@gmail.com>
2022-05-28 09:32:44 +0700

    update database configuration for production

--- app/config/config.php
+++ app/config/config.php
@@ -2,15 +2,15 @@
 
 //Database params
 define('DB_HOST', 'localhost'); //Add your db host
-define('DB_USER', 'root'); // Add your DB root
-define('DB_PASS', ''); //Add your DB pass
-define('DB_NAME', 'lampstack_db'); //Add your DB Name
+define('DB_USER', 'u514962216_rezaduha'); // Add your DB root
+define('DB_PASS', '1K!L0x9^:o'); //Add your DB pass
+define('DB_NAME', 'u514962216_lampstack_db'); //Add your DB Name
 
 //APPROOT
 define('APPROOT', dirname(dirname(__FILE__)));
 
 //URLROOT (Dynamic links)
-define('URLROOT', 'http://localhost/lampstack');
+define('URLROOT', 'http://lampstackweb.site');
 
 //Sitename
 define('SITENAME', 'LAMP Stack');
