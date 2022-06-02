3c011e2b3f6d06629f6811c88f67293348c11617
rezaduha <rezafauzizisiwaduha@gmail.com>
2022-06-02 19:11:57 +0700

    rearrange tags inside head

--- app/views/includes/head.php
+++ app/views/includes/head.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="<?= URLROOT ?>/public/favicon.ico" type="image/x-icon" />
-    <title>LAMP Stack | <?= $data['metaTitle'] ?? "Home" ?></title>
     <meta name="description" content="<?= $data['metaDesc'] ?? "LAMP Stack Website" ?>" />
+    <title>LAMP Stack | <?= $data['metaTitle'] ?? "Home" ?></title>
+    <link rel="shortcut icon" href="<?= URLROOT ?>/public/favicon.ico" type="image/x-icon" />
     <link rel="stylesheet" href="<?= URLROOT ?>/public/assets/css/main.css">
     <!-- <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" /> -->
     <link rel="stylesheet" href="<?= URLROOT ?>/public/assets/css/swiper-bundle.min.css">
