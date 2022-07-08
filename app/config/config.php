a9ad48cd9377268405091be901e6f286eaa9855f
rezaduha <rezafauzizisiwaduha@gmail.com>
2022-07-08 10:36:34 +0700

    change db name to optimized db

--- app/config/config.php
+++ app/config/config.php
@@ -4,7 +4,7 @@
 define('DB_HOST', 'localhost'); //Add your db host
 define('DB_USER', 'root'); // Add your DB root
 define('DB_PASS', 'Admin_220622!'); //Add your DB pass
-define('DB_NAME', 'lampstack_db'); //Add your DB Name
+define('DB_NAME', 'lampstack_db_optimized'); //Add your DB Name
 
 //APPROOT
 define('APPROOT', dirname(dirname(__FILE__)));
