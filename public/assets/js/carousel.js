a3dca2c9c2802a947c347a8ae54d41fd16fee5ad
rezaduha <rezafauzizisiwaduha@gmail.com>
2022-07-10 13:13:40 +0700

    minify js

--- public/assets/js/carousel.js
+++ public/assets/js/carousel.js
@@ -1,16 +1,1 @@
-var carousel = new Swiper(".featured-topic__carousel", {
-  slidesPerView: 1.4,
-  spaceBetween: 42,
-  loop: true,
-  speed: 800,
-  breakpoints: {
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 86,
-    },
-  },
-  navigation: {
-    nextEl: ".slick-next",
-    prevEl: ".slick-prev",
-  },
-});
+var carousel=new Swiper(".featured-topic__carousel",{slidesPerView:1.4,spaceBetween:42,loop:!0,speed:800,breakpoints:{768:{slidesPerView:3,spaceBetween:86}},navigation:{nextEl:".slick-next",prevEl:".slick-prev"}})
